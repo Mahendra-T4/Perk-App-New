36f836ad8c0c8e361d697e20741155469c01839d
Mahendra Kuldeep-T4 <mahendra.tfourtech@gmail.com>
2026-05-22 09:58:12 +0530

    update repo

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:perk_app/app.dart';
 import 'package:perk_app/core/config/env_config.dart';
 import 'package:perk_app/core/service/device_id.dart';
 
+//main file
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Don't await here - fetch async after app starts
